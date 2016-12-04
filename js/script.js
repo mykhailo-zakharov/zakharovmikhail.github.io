636c8f640c1c99322e5c353be4f1228773c703be
Михаил Захаров < >
2016-12-04 12:58:19 +0200

    fix

--- js/script.js
+++ js/script.js
@@ -2,7 +2,9 @@ function Task(){
 	var self = this;
 	this.init = function(){
 		self.menu();
-
+		if( $(".works").length ){
+			this.gallery();
+		}
 
 	}
    	this.menu = function(){
@@ -10,6 +12,37 @@ function Task(){
 	   			$(this).toggleClass("menu-on");
 	   			$(".headerContent").slideToggle();
 		});
+   	}
+   	this.gallery = function(){
+
+   		self.setLargeImg(0);
+
+   		$(".worksWrapMiniatureImg").click(function(){
+   			// var eq = ;
+   			$(".worksWrapLargeImg").attr( "src", $(this).data("src") ).data("src", $(this).parent().index() );
+   			$(".worksDescribe").text( $(this).data("describe") );
+   		});
+
+   		$(".btnNext").click(function(){
+			var newEq = +$(".worksWrapLargeImg").data("eq") + 1;
+			if( newEq == $(".worksWrapMiniatureImg").length ){
+				newEq = 0;
+			}
+	   		self.setLargeImg( newEq );
+   		});
+
+   		$(".btnPrev").click(function(){
+			var newEq = +$(".worksWrapLargeImg").data("eq") - 1;
+			if( newEq == 0 ){
+				newEq = $(".worksWrapMiniatureImg").length;
+			}
+	   		self.setLargeImg( newEq );
+   		});
+   	}
+   	this.setLargeImg = function(eq){
+   		var elem = $(".worksWrapMiniatureImg").eq( eq );
+   		$(".worksWrapLargeImg").attr("src", elem.data("src") ).data("eq", eq);
+   		$(".worksDescribe").text( elem.data("describe") );
    	}
 
     this.init();
