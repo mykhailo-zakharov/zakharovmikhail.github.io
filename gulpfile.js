b644cfde0928d44d382d2301992144103f9c981f
Zakharov Mikhail <izakharovm@gmail.com>
2017-01-20 10:53:07 +0200

    fix

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,7 @@ var browserSync = require('browser-sync');
 var gulp = require('gulp'),
 sass = require('gulp-sass'),
 uglify = require('gulp-uglify'),
-// inlinesource = require('gulp-inline-source'),
 jade = require('gulp-jade');
-
-
 
 gulp.task('jade', function() {
         gulp.src('gulp/*.jade')
@@ -14,20 +11,15 @@ gulp.task('jade', function() {
         .pipe(gulp.dest('./raw'));
 
         gulp.src('gulp/**/*.jade')
-        .pipe(jade({pretty: false}))
+        .pipe(jade({pretty: true}))
         .pipe(gulp.dest(''))
         .pipe(browserSync.reload({
                 stream: true
         }));
 });
 
-
 gulp.task('sass', function () {
         return gulp.src('gulp/css/*.scss')
-        // .pipe(sass().on('error', sass.logError))
-        // .pipe(gulp.dest('./css')).pipe(csso());
-        // .pipe(gulp.dest('css'));
-
         .pipe(sass())
         .pipe(gulp.dest('css'))
         .pipe(browserSync.reload({
@@ -40,14 +32,6 @@ gulp.task('js', function () {
         .pipe(uglify())
         .pipe(gulp.dest('./js'));
 });
-
-// gulp.task('inline-code', function () {
-//     return gulp.src('raw')
-//         .pipe(inlinesource(compress: false))
-//         .pipe(gulp.dest(''));
-
-
-// });
 
 gulp.task('browserSync', function() {
   browserSync({
@@ -63,13 +47,5 @@ gulp.task('watch', ['browserSync'], function() {
         gulp.watch('./gulp/js/**/*.js', ['js']);
 
 });
-
-// gulp.task('watch', ['browserSync', 'sass'], function (){
-//   gulp.watch('app/scss/**/*.scss', ['sass']); 
-//   // Обновляем браузер при любых изменениях в HTML или JS
-//   gulp.watch('app/*.html', browserSync.reload); 
-//   gulp.watch('app/js/**/*.js', browserSync.reload); 
-// });
-
 
 gulp.task('default', ['watch']);
